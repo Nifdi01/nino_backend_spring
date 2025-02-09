c5d1102ad6d9448e10a5932b2efb373a2504c1c3
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2025-02-09 13:50:34 +0400

    Added JWT Utilites and Security configurations

---
+++ src/main/java/ai/tifosi/nino_backend_spring/util/JwtUtils.java
@@ -0,0 +1,39 @@
+package ai.tifosi.nino_backend_spring.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.accessExpirationMs}")
+    private Long accessExpirationMs;
+
+    @Value("${jwt.refreshExpirationMs}")
+    private Long refreshExpirationMs;
+
+    public String generateAccessToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
